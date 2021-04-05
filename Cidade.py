181ed9cf2ccf42b5281a4cc26b60f49f267d8b00
Gustavo <guriposa@gmail.com>
2021-04-05 16:48:52 -0300

    melhoria nomes metodos e inicio de implementacoes dijkstra e A*

--- Cidade.py
+++ Cidade.py
@@ -1,8 +1,9 @@
 class Cidade:
-    def __init__(self, nome):
+    def __init__(self, nome,distanciaObjetivo):
         self.nome = nome
         self.visitado = False
         self.adjacentes = []
+        self.distanciaObjetivo = distanciaObjetivo # heurística usando distancia em linha reta entre as duas cidades
         
     def addCidadeAdjacente(self,cidade):
         self.adjacentes.append(cidade)
